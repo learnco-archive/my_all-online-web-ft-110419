d70e1d3aec82ad8665f3980955a9d20cdc56ede8
Jess Fischbach <robotspacefish@gmail.com>
2019-11-06 19:38:55 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,10 +3,10 @@ require 'pry'
 def my_all?(collection)
   i = 0
   while i < collection.length
-    binding.pry
+
     yield(collection[i])
     i += 1
   end
 end
-
+binding.pry
 my_all?([1,2,3]) { |i| i < 2 }
